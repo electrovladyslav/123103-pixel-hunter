9fd8c35d9e834185c9f7e6bfc965c7136527413b
Yermolin Vlad <electrovladyslav@gmail.com>
2017-05-26 13:31:54 +0300

    Added use strict & IIFE by mentors remark.

--- js/main.js
+++ js/main.js
@@ -1,46 +1,46 @@
-// ***TASK #2***
-const templates = document.querySelectorAll(`template`);
-const main = document.querySelector(`main`);
-let currentScreen;
+(function () {
+  'use strict';
 
-// ***TASK #3***
-/**
- *Switching screens
- *
- * @param {number} screenNumber Screen to which will switch
- */
-function switchScreen(screenNumber) {
-  if ((screenNumber < 0) || (screenNumber > templates.length - 1)) {
-    return;
-  }
+  const templates = document.querySelectorAll(`template`);
+  const main = document.querySelector(`main`);
+  let currentScreen;
 
-  const clone = document.importNode(templates[screenNumber].content, true);
-  main.innerHTML = ``;
-  main.appendChild(clone);
-  currentScreen = screenNumber;
-}
-
-// ***TASK #4***
-switchScreen(0);
-
-// ***TASK #5***
-/**
- * Hadler for keyboards events
- * @param event
- */
-
-function handler(event) {
-  if (event.altKey) {
-    if (event.key === `ArrowRight`) {
-      switchScreen(currentScreen + 1);
-      event.preventDefault();
+  /**
+   *Switching screens
+   *
+   * @param {number} screenNumber Screen to which will switch
+   */
+  function switchScreen(screenNumber) {
+    if ((screenNumber < 0) || (screenNumber > templates.length - 1)) {
+      return;
     }
 
-    if (event.key === `ArrowLeft`) {
-      switchScreen(currentScreen - 1);
-      event.preventDefault();
+    const clone = document.importNode(templates[screenNumber].content, true);
+    main.innerHTML = ``;
+    main.appendChild(clone);
+    currentScreen = screenNumber;
+  }
+
+  switchScreen(0);
+
+  /**
+   * Hadler for keyboards events
+   * @param event
+   */
+
+  function handler(event) {
+    if (event.altKey) {
+      if (event.key === `ArrowRight`) {
+        switchScreen(currentScreen + 1);
+        event.preventDefault();
+      }
+
+      if (event.key === `ArrowLeft`) {
+        switchScreen(currentScreen - 1);
+        event.preventDefault();
+      }
     }
   }
-}
 
-document.addEventListener(`keyup`, handler);
+  document.addEventListener(`keyup`, handler);
+})();
