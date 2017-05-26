3f473c230089a59fb34acfae42b02a67b70efd9d
Yermolin Vlad <electrovladyslav@gmail.com>
2017-05-26 08:40:14 +0300

    Subtask 5 of task 2 was done - screen switching by keyboard arrows.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,46 @@
-const templates = document.getElementsByTagName(`TEMPLATE`);
-const main = document.getElementsByTagName(`MAIN`)[0];
+// ***TASK #2***
+const templates = document.querySelectorAll(`template`);
+const main = document.querySelector(`main`);
+let currentScreen;
 
-function showScreen(templateNumber) {
-  const clone = document.importNode(templates[templateNumber].content, true);
+// ***TASK #3***
+/**
+ *Switching screens
+ *
+ * @param {number} screenNumber Screen to which will switch
+ */
+function switchScreen(screenNumber) {
+  if ((screenNumber < 0) || (screenNumber > templates.length - 1)) {
+    return;
+  }
+
+  const clone = document.importNode(templates[screenNumber].content, true);
   main.innerHTML = ``;
   main.appendChild(clone);
+  currentScreen = screenNumber;
 }
 
-showScreen(0);
+// ***TASK #4***
+switchScreen(0);
+
+// ***TASK #5***
+/**
+ * Hadler for keyboards events
+ * @param event
+ */
+
+function handler(event) {
+  if (event.altKey) {
+    if (event.key === `ArrowRight`) {
+      switchScreen(currentScreen + 1);
+      event.preventDefault();
+    }
+
+    if (event.key === `ArrowLeft`) {
+      switchScreen(currentScreen - 1);
+      event.preventDefault();
+    }
+  }
+}
+
+document.addEventListener(`keyup`, handler);
