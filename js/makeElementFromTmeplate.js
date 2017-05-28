d1486450a1852df4a4a21aa3da3122e4635863bc
Yermolin Vlad <electrovladyslav@gmail.com>
2017-05-28 18:46:26 +0300

    Task 2.3-4: All templates were removed from index to modules, functions to make element from temlate and switch screens also.

--- js/makeElementFromTmeplate.js
+++ js/makeElementFromTmeplate.js
@@ -4,18 +4,11 @@
  * @returns {Element} DOM element
  */
 
-// const myDiv = makeElementFromTemplate(`<div>Разметка DOM-элемента</div>`);
+// const myDiv = makeElementFromTemplate(`<div id='test'>Разметка DOM-элемента</div>`);
 const makeElementFromTemplate = (tmplString) => {
-  const endOpenTag = tmplString.indexOf(`\>`);
-  const startCloseTag = tmplString.lastIndexOf(`\<\/`);
-
-  const tagName = tmplString.slice(1, endOpenTag);
-  const content = tmplString.slice(endOpenTag + 1, startCloseTag);
-
-  let DOMElem = document.createElement(`${tagName}`);
-  DOMElem.innerHTML = content;
-
-  return DOMElem;
+  const template = document.createElement(`template`);
+  template.innerHTML = tmplString;
+  return template.content;
 };
 
 export default makeElementFromTemplate;
---
+++ js/showScreen.js
@@ -0,0 +1,19 @@
+const main = document.querySelector(`main`);
+
+const showScreen = (element) => {
+
+  main.innerHTML = ``;
+
+  main.appendChild(element);
+
+  // а через клонирование не работает подписка на события
+  /* const clone = element.cloneNode(true);
+  container.appendChild(clone);*/
+
+  /* elements.forEach((item) => {
+    const clone = document.importNode(item, true);
+    container.appendChild(clone);
+  });*/
+};
+
+export default showScreen;
